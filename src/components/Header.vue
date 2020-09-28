86fb03efd275d706f22cc1b19b14460aa9c820bf
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-09-28 21:20:08 +0200

    added animated highlight for selected link in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,30 @@
     <div class="centering-container">
       <img src="../assets/images/logo.png" />
       <nav>
-        <div class="link"><router-link to="/">Home</router-link></div>
-        <div class="link"><router-link to="/menu">Menu</router-link></div>
-        <div class="link">
+        <div
+          class="link"
+          :class="{ 'router-link-exact-active': $route.path === '/' }"
+        >
+          <router-link to="/">Home</router-link>
+        </div>
+        <div
+          class="link"
+          :class="{ 'router-link-exact-active': $route.path === '/menu' }"
+        >
+          <router-link to="/menu">Menu</router-link>
+        </div>
+        <div
+          class="link"
+          :class="{ 'router-link-exact-active': $route.path === '/delivery' }"
+        >
           <router-link to="/delivery">Delivery</router-link>
         </div>
-        <div class="link"><router-link to="/about">About</router-link></div>
+        <div
+          class="link"
+          :class="{ 'router-link-exact-active': $route.path === '/about' }"
+        >
+          <router-link to="/about">About</router-link>
+        </div>
       </nav>
     </div>
   </header>
@@ -31,14 +49,49 @@
       display: flex;
 
       .link {
+        padding: 1rem 0.5rem;
+        &.router-link-exact-active {
+          &::after {
+            content: "";
+            display: block;
+            width: 80%;
+            height: 2px;
+            margin: auto;
+            margin-top: 5px;
+            background-color: rgb(223, 91, 4);
+            animation: link-underline 0.5s cubic-bezier(0, 0.8, 1, 0.85);
+          }
+          a {
+            color: rgb(223, 91, 4);
+          }
+        }
         a {
           color: rgb(70, 22, 22);
           margin: 1rem;
           font-size: 1.2rem;
           text-decoration: none;
+          transition: 0.5s;
         }
       }
     }
+  }
+}
+
+@keyframes link-underline {
+  0% {
+    transform: translateY(5px);
+  }
+  70% {
+    transform: translateY(-5px);
+  }
+  80% {
+    transform: translateY(2px);
+  }
+  90% {
+    transform: translateY(-2px);
+  }
+  100% {
+    transform: translateY(0);
   }
 }
 </style>
