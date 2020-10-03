07f42a21ae59ed5d40757b13f5f6525a2ebf74c4
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-10-03 20:31:18 +0200

    started working on menu page and created basic menu-item component

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="menu-item">
+    <h3 class="title">{{ name }}</h3>
+    <div class="content">
+      <img :src="imageSrc" />
+      <p class="price">price: {{ price }}$</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "imageSrc", "price"],
+};
+</script>
+
+<style lang="scss" scoped>
+.menu-item {
+  display: inline-block;
+  width: 15rem;
+  padding: 1rem;
+  border: 2px solid rgb(70, 22, 22);
+  border-radius: 10px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+img {
+  width: 80%;
+  height: 8rem;
+}
+
+.title {
+  margin: 0 0 10px 0;
+  color: rgb(223, 91, 4);
+}
+
+.price {
+  margin: 10px 0 0 0;
+  align-self: flex-end;
+}
+</style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="menu">
-    <h1>Menu</h1>
+    <div class="centering-container">
+      <h1>Menu</h1>
+      <section>
+        <h2>Classic Pizzas</h2>
+        <menu-item
+          name="Margerita"
+          :imageSrc="require('../assets/images/info-generic-pizza.jpg')"
+          :price="10"
+        />
+      </section>
+    </div>
   </div>
 </template>
+
+<script>
+import MenuItem from "../components/MenuItem";
+export default {
+  components: {
+    "menu-item": MenuItem,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
