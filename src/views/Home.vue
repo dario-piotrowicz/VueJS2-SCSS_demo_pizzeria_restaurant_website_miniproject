15d32a0f817f838d5ca921b3c00313ee2774370e
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-10-03 16:46:55 +0200

    implemented generic-info-card for oven (still work-in-progress)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,18 +16,23 @@
       </div>
     </hero>
     <generic-info-card
-      :imageSrc="require('../assets/images/generic-info-1.jpg')"
+      :imageSrc="require('../assets/images/info-generic-pizza.jpg')"
       title="Real Italian Pizza"
       description="Our pizzas are made with only the best ingredients and cooked as per the traditional italian repices. We pride ourself in our food quality and guarantee you that you won't be dissapointed to eat our pizzas."
     />
     <generic-info-card
-      :imageSrc="require('../assets/images/generic-info-delivery.jpg')"
+      :imageSrc="require('../assets/images/info-delivery.jpg')"
       title="Delivery"
       description="We provide a delivery service which brings our delicious pizzas right in front of your door, still warm and ready to eat. All you need to do is call us and place your order, our staff will see that your pizzas are delivered right on time."
       :image-on-the-right="true"
     >
       <router-link class="btn delivery" to="/delivery">Read More</router-link>
     </generic-info-card>
+    <generic-info-card
+      :imageSrc="require('../assets/images/info-oven.jpg')"
+      title="Only Wood-Fired Ovens"
+      description="Our pizzas are only cooked in real classic wood-fired ovens, these guarantee a more omogeneous heat distribution which produces richer smoky flavors and a very nice crispy crust not obtainable using moder ovens."
+    />
   </div>
 </template>
 
