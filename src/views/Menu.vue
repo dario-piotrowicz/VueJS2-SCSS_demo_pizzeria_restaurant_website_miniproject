0b258b7ede2f58a59f678b82771ab4ccd873194a
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-10-04 12:08:52 +0200

    added pepperoni and quattro-formaggi to menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -6,9 +6,21 @@
         <h2>Classic Pizzas</h2>
         <menu-item
           name="Margherita"
-          :imageSrc="require('../assets/images/info-generic-pizza.jpg')"
+          :imageSrc="require('../assets/images/menu/margherita.jpg')"
           :price="10"
           description="The most classic Neapolitan pizza, made with tomatoes sauce, mozzarella cheese and a touch of fresh basil."
+        />
+        <menu-item
+          name="Pepperoni"
+          :imageSrc="require('../assets/images/menu/pepperoni.jpg')"
+          :price="12"
+          description="One of the most popular pizza, made with a standard base of tomatoes sauce and mozzarella, and completed by slices of delicious pepperoni."
+        />
+        <menu-item
+          name="Quattro Formaggi"
+          :imageSrc="require('../assets/images/menu/quattro-formaggi.jpg')"
+          :price="12"
+          description="Quattro formaggi in italian literally means 'four cheeses'. Those are places on top of the standard pizza base and comprise of Mozzarella, Gorgonzola, Fontina and Parmigiano."
         />
       </section>
     </div>
