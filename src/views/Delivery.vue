4b81e44dafdf67f7f17cb327c58025910063034a
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-10-10 17:20:07 +0200

    added top and bottom margins to the delivery-steps section

--- src/views/Delivery.vue
+++ src/views/Delivery.vue
@@ -2,41 +2,43 @@
   <div id="delivery">
     <div class="centering-container">
       <h1 class="title">Delivery</h1>
-      <delivery-step icon="clipboard-list">
-        <div class="delivery-step-content">
-          <span>Choose from our </span
-          ><router-link to="/menu">Menu</router-link>
-        </div>
-      </delivery-step>
-      <div class="delivery-steps-divider" />
-      <delivery-step icon="dollar-sign">
-        <div class="delivery-step-content">
-          <p>Note that our delivery fee is 15$</p>
-          <p class="details">(30$ for more then 10 pizzas)</p>
-        </div>
-      </delivery-step>
-      <div class="delivery-steps-divider" />
-      <delivery-step icon="map-signs">
-        <div class="delivery-step-content">
-          <p>And that we deliver in a 5 miles range from our location</p>
-          <p class="details">
-            ( Pizzas Rd, Somewhere , 12 - P1ZZ4 United Kingdom )
-          </p>
-        </div>
-      </delivery-step>
-      <div class="delivery-steps-divider" />
-      <delivery-step icon="phone-alt">
-        <div class="delivery-step-content">
-          <p>Call us at +12 3456 7891011 and place your order</p>
-        </div>
-      </delivery-step>
-      <div class="delivery-steps-divider" />
-      <delivery-step icon="pizza-slice">
-        <div class="delivery-step-content">
-          <p>Receive and pay for your pizzas</p>
-          <p class="details">(we accept either cash or card)</p>
-        </div>
-      </delivery-step>
+      <div id="delivery-steps-container">
+        <delivery-step icon="clipboard-list">
+          <div class="delivery-step-content">
+            <span>Choose from our </span
+            ><router-link to="/menu">Menu</router-link>
+          </div>
+        </delivery-step>
+        <div class="delivery-steps-divider" />
+        <delivery-step icon="dollar-sign">
+          <div class="delivery-step-content">
+            <p>Note that our delivery fee is 15$</p>
+            <p class="details">(30$ for more then 10 pizzas)</p>
+          </div>
+        </delivery-step>
+        <div class="delivery-steps-divider" />
+        <delivery-step icon="map-signs">
+          <div class="delivery-step-content">
+            <p>And that we deliver in a 5 miles range from our location</p>
+            <p class="details">
+              ( Pizzas Rd, Somewhere , 12 - P1ZZ4 United Kingdom )
+            </p>
+          </div>
+        </delivery-step>
+        <div class="delivery-steps-divider" />
+        <delivery-step icon="phone-alt">
+          <div class="delivery-step-content">
+            <p>Call us at +12 3456 7891011 and place your order</p>
+          </div>
+        </delivery-step>
+        <div class="delivery-steps-divider" />
+        <delivery-step icon="pizza-slice">
+          <div class="delivery-step-content">
+            <p>Receive and pay for your pizzas</p>
+            <p class="details">(we accept either cash or card)</p>
+          </div>
+        </delivery-step>
+      </div>
     </div>
   </div>
 </template>
@@ -77,11 +79,16 @@ export default {
   }
 }
 
-.delivery-steps-divider {
-  height: 10rem;
-  width: 0.5rem;
-  margin: 3rem auto;
-  background-color: rgb(70, 22, 22);
-  border-radius: 5px;
+#delivery-steps-container {
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+
+  .delivery-steps-divider {
+    height: 10rem;
+    width: 0.5rem;
+    margin: 3rem auto;
+    background-color: rgb(70, 22, 22);
+    border-radius: 5px;
+  }
 }
-</style>>
+</style>
