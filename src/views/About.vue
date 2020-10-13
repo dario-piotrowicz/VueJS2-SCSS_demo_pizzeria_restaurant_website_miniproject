0616af2443ab144530c0df8f675ed2f073c7a5a4
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-10-13 21:06:57 +0200

    started setting app about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>About</h1>
+  <div id="about">
+    <div class="centering-container title-container">
+      <h1 class="title">About</h1>
+    </div>
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+.title {
+  text-align: center;
+  font-size: 2.2rem;
+}
+</style>
