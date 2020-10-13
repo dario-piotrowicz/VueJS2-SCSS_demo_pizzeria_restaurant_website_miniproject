0edd8edd86015291e9137c105581fdb4f9a0570b
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-10-13 23:04:14 +0200

    made initial skeleton for initial section of about page (work-in-progress)

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,107 @@
     <div class="centering-container title-container">
       <h1 class="page-title">About</h1>
     </div>
+    <div class="full-width-wrapper">
+      <div class="basic-info-container centering-container">
+        <div class="contact-info">
+          <div class="info">
+            <p>HOME_ICON</p>
+            <p>address</p>
+          </div>
+          <div class="info">
+            <p>PHONE_ICON</p>
+            <p>phone number</p>
+          </div>
+          <div class="info">
+            <p>EMAIL_ICON</p>
+            <p>email addr</p>
+          </div>
+        </div>
+        <div class="opening-times-container">
+          <h2 class="opening-times">Opening Times</h2>
+          <div class="times-info">
+            <div class="day-of-week">Monday:</div>
+            <div class="times">11:00 - 16:00, 19:00 - 23:30</div>
+          </div>
+          <div class="times-info">
+            <div class="day-of-week">Tuesday:</div>
+            <div class="closed">Closed</div>
+          </div>
+          <div class="times-info">
+            <div class="day-of-week">Wednesday:</div>
+            <div class="times">11:00 - 16:00, 19:00 - 23:30</div>
+          </div>
+          <div class="times-info">
+            <div class="day-of-week">Thursday:</div>
+            <div class="times">11:00 - 16:00, 19:00 - 23:30</div>
+          </div>
+          <div class="times-info">
+            <div class="day-of-week">Friday:</div>
+            <div class="times">11:00 - 16:00, 19:00 - 23:30</div>
+          </div>
+          <div class="times-info">
+            <div class="day-of-week">Saturday:</div>
+            <div class="times">11:00 - 16:00, 19:00 - 23:30</div>
+          </div>
+          <div class="times-info">
+            <div class="day-of-week">Sunday:</div>
+            <div class="times">11:00 - 16:00, 19:00 - 23:30</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.full-width-wrapper {
+  background-color: #fff;
+  padding: 2rem;
+  margin: 2rem 0;
+
+  .basic-info-container {
+    display: flex;
+    justify-content: space-around;
+
+    .contact-info,
+    .opening-times-container {
+      padding: 2rem;
+      border: 1px solid red;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+}
+
+.times-info {
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem 0;
+
+  .day-of-week {
+    font-weight: bolder;
+  }
+
+  .times,
+  .closed {
+    width: 11rem;
+    margin-left: 1rem;
+  }
+
+  .times {
+    text-align: end;
+  }
+
+  .closed {
+    text-align: center;
+  }
+}
+
+.contact-info {
+  .info {
+    padding: 2rem;
+    border: 1px solid blue;
+  }
+}
+</style>
