66686571aa858e35c384b16a12ba1d38a5e9b308
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-11-15 18:41:23 +0100

    modified PageNotFound component to show correctly in dark mode

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="page-not-found">
+  <div id="page-not-found" class="page" v-darkmode>
     <h1>404 - Page Not Found</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/constants.scss";
+
+#page-not-found {
+  height: calc(100vh - 346px);
+  box-sizing: border-box;
+
+  &.dark-mode {
+    color: $dark-mode--color-primary;
+  }
+}
+</style>
